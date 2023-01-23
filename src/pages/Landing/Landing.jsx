8666b082d2bdd726f6f9def2aad2bc2dae12d64f
Enock <enock.mokua@student.moringaschool.com>
2023-01-23 15:16:13 +0300

    add content to landin gpage

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -3,8 +3,20 @@ import "./landing.css";
 export default function Landing() {
   return (
     <main id="landing">
-      <div id="landing-container1">landing contaner 1</div>
-      <div id="landing-container2">Landing container 2</div>
+      <div id="landing-container1">
+        <h2>
+          No matter the quality or quantity of data we will automate the process
+          for you.
+        </h2>
+
+        <p>
+          We are focused on foing the heay lifting for you so that you can focus
+          on building your business.
+        </p>
+      </div>
+      <div id="landing-container2">
+        <button>Get Started for free</button>
+      </div>
     </main>
   );
 }
