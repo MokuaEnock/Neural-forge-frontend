aa2bf772a9e7fa846ac84a7841d22c6521318549
Enock <enock.mokua@student.moringaschool.com>
2023-01-23 14:32:04 +0300

    set up default structure for landing container

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,5 +1,10 @@
 import "./landing.css";
 
 export default function Landing() {
-  return <main>Landing page</main>;
+  return (
+    <main id="landing">
+      <div id="landing-container1">landing contaner 1</div>
+      <div id="landing-container2">Landing container 2</div>
+    </main>
+  );
 }
