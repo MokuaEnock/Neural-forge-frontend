6387b213d1e6cf8bc609256eb8bc56e06b498bcb
Enock <enock.mokua@student.moringaschool.com>
2023-01-23 17:40:07 +0300

    style form submit form

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -5,7 +5,9 @@ export default function User() {
     <main id="user">
       <form>
         <label for="avatar">Choose a dataset:</label>
-        <input type="file" name="avatar" accept="image/png, image/jpeg"></input>
+        <input type="file" name="avatar" accept=".csv"></input>
+
+        <button type="submit">Generate Report</button>
       </form>
     </main>
   );
