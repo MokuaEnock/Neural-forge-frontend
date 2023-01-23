499f18e330c824b2db95c25f0800c13d36eee8c9
Enock <enock.mokua@student.moringaschool.com>
2023-01-23 16:37:24 +0300

    add links to navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,3 +1,9 @@
+import { Link } from "react-router-dom";
 export default function NavBar() {
-  return <nav>Navigation</nav>;
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/user">User</Link>
+    </nav>
+  );
 }
