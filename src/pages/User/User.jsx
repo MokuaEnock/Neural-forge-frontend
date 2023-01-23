752ac405960b732e6142c6172a3faa8f3b3cc706
Enock <enock.mokua@student.moringaschool.com>
2023-01-23 17:03:50 +0300

    add form to upload file

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -1,5 +1,13 @@
 import "./User.css";
 
 export default function User() {
-  return <main></main>;
+  return (
+    <main id="user">
+      <form>
+        <label>Upload your dataset</label>
+        <input type="file" accept=".csv" />
+        <input type="submit" value="Upload" />
+      </form>
+    </main>
+  );
 }
