486bf31a7032550c919dc5041d56f05d7a83803a
Enock <enock.mokua@student.moringaschool.com>
2023-01-23 14:44:16 +0300

    configure app routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Landing from "./pages/Landing/Landing";
 import Footer from "./components/Footer";
 import NavBar from "./components/Nav";
 
 function App() {
   return (
-    <>
-      <Landing />
-    </>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
