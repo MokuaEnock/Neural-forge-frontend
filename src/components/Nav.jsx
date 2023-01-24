23bca86cdaed4075f8e46b06bfd00cbd699b9093
Enock <enock.mokua@student.moringaschool.com>
2023-01-24 12:40:49 +0300

    create scrape form and add basic styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,6 +4,7 @@ export default function NavBar() {
     <nav>
       <Link to="/">Home</Link>
       <Link to="/user">User</Link>
+      <Link to="/scrape">Scrape</Link>
     </nav>
   );
 }
--- src/pages/Scrape/Scrape.jsx
+++ src/pages/Scrape/Scrape.jsx
@@ -1,5 +1,14 @@
 import "./Scrape.css";
 
 export default function Scrape() {
-  return <main>Scrape data from a website</main>;
+  return (
+    <main id="scrape">
+      <form>
+        <p>Paste the link to the website</p>
+        <input type="url" placeholder="Paste your link here" />
+        <input type="number" placeholder="Number of pages" />
+        <button type="submit">Submit</button>
+      </form>
+    </main>
+  );
 }
