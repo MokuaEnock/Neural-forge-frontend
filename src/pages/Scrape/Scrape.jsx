42a5c290d7d1d5b0a980899469359066be82cbc9
Enock <enock.mokua@student.moringaschool.com>
2023-01-24 12:44:16 +0300

    add function to handle scrape form submit

--- src/pages/Scrape/Scrape.jsx
+++ src/pages/Scrape/Scrape.jsx
@@ -1,9 +1,14 @@
 import "./Scrape.css";
 
 export default function Scrape() {
+  function handleScrape(e) {
+    e.preventDefault();
+    console.log("Scrape");
+  }
+
   return (
     <main id="scrape">
-      <form>
+      <form onSubmit={handleScrape}>
         <p>Paste the link to the website</p>
         <input type="url" placeholder="Paste your link here" />
         <input type="number" placeholder="Number of pages" />
